4d16009eb670b488b71a179fff0260dfd9e1dfad
Serhiy Onyshchenko <serhiy@Serhiys-Laptop.local>
2022-11-23 09:35:06 +0100

    LVIV-2 making sure to store idea prefs

--- idea.sh
+++ idea.sh
@@ -1,8 +1,11 @@
 #! /bin/sh
 #  -e DISPLAY=${DISPLAY} \
+IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+D="$IP:0"
 docker run --rm \
-  -e DISPLAY=${DISPLAY} \
+  -e DISPLAY=${D} \
   -v ~/.Idea:/home/developer/.Idea \
+  -v ~/.Idea.config:/home/developer/.config \
   -v ~/.Idea.java:/home/developer/.java \
   -v ~/.Idea.coursier:/home/developer/.coursier \
   -v ~/.Idea.ivy:/home/developer/.ivy2 \
@@ -12,7 +15,7 @@ docker run --rm \
   -v ~/.Idea.share:/home/developer/.local/share/JetBrains \
   -v ~/dev:/home/developer/dev \
   --name idea-$(head -c 4 /dev/urandom | xxd -p)-$(date +'%Y%m%d-%H%M%S') \
-ziohimself/exalate-dev-env:latest sleep infinity
+ziohimself/exalate-dev-env:latest 
 
   #--network host \
   #-v /tmp/:/tmp/ \
