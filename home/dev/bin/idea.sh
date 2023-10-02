f6fa70a958a114aecd6a9de434649ba32cd8e025
Yves Hanoulle (he/him) <yveshanoulle@users.noreply.github.com>
2023-10-02 12:56:27 +0200

    Update idea.sh
    remove unnecessary Db_IP initialisation.

--- home/dev/bin/idea.sh
+++ home/dev/bin/idea.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 export D="host.docker.internal:0"
-export DB_IP=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgres_appnetwork_1`
+#export DB_IP=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgres_appnetwork_1`
 xhost +localhost
 docker-compose -f ~/dev/bin/docker-compose.yml up
 
