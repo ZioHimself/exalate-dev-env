8f6ec20156c90aee0ace3535d8f726a1a4689637
Yves Hanoulle (he/him) <yveshanoulle@users.noreply.github.com>
2023-10-02 13:23:20 +0200

    Update idea.sh
    add verbose flag to debug faulty images

--- home/dev/bin/idea.sh
+++ home/dev/bin/idea.sh
@@ -2,5 +2,5 @@
 export D="host.docker.internal:0"
 #export DB_IP=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' postgres_appnetwork_1`
 xhost +localhost
-docker-compose -f ~/dev/bin/docker-compose.yml up
+docker-compose --verbose -f ~/dev/bin/docker-compose.yml up
 
